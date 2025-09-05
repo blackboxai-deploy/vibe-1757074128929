68eeee294968771ffa220f6e7abb3414806601d0
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-05 17:39:02 +0530

    Add src/App.vue

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <router-view />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
